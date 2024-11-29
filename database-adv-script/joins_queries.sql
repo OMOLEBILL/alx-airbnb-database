42117866d32e3bc725d5b5ae8965e72c68c010f5
omole otieno <omolebill01@gmail.com>
2024-11-29 23:23:49 +0100

    Ordered the retrieved property using id

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -26,6 +26,8 @@ FROM
     Property
 LEFT JOIN 
     Review ON Property.property_id = Review.property_id;
+ORDER BY 
+    Property.property_id;
 
 -- Query using FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 SELECT 
