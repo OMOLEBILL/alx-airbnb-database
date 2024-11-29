29a619cb38baf8c50d913443511f241f67724491
omole otieno <omolebill01@gmail.com>
2024-11-29 23:46:57 +0100

    used the aggregation count function and Rank function

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,26 @@
+-- Aggregation query to find the total number of bookings made by each user
+SELECT 
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    COUNT(Booking.booking_id) AS total_bookings
+FROM 
+    User
+LEFT JOIN 
+    Booking ON User.user_id = Booking.user_id
+GROUP BY 
+    User.user_id, User.first_name, User.last_name;
+
+-- Window function to rank properties based on the total number of bookings they have received
+SELECT 
+    Property.property_id,
+    Property.name,
+    Property.location,
+    COUNT(Booking.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(Booking.booking_id) DESC) AS rank
+FROM 
+    Property
+LEFT JOIN 
+    Booking ON Property.property_id = Booking.property_id
+GROUP BY 
+    Property.property_id, Property.name, Property.location;
